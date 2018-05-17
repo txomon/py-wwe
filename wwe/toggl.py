1daca7f128eec42dffffe9126fb6b13fda277762
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-05-17 09:22:21 +0100

    WIP

---
+++ wwe/toggl.py
@@ -0,0 +1,28 @@
+from tapioca_toggl import Toggl
+from datetime import datetime
+
+api = Toggl(access_token="b0769e321caa66ba4cdb946598ba6e88")
+
+me = api.me_with_related_data().get().data()
+
+print(me)
+
+
+class TogglWrap:
+    def __init__(self, token=""):
+        if token != "" and token is not None:
+            print("jey! token received! :D")
+            self.toggl = Toggl(access_token=token)
+            print("Toggl client wrapper created!")
+
+    def get_tasks(self, start: datetime, end: datetime, client: str):
+        """Return tasks from Toggl between two given dates for a client"""
+        if self.toggl is not None:
+            print("fetching your tasks!")
+
+
+t = TogglWrap(token="b0769e321caa66ba4cdb946598ba6e88")
+start_date = datetime(2018, 2, 5)
+end_date = datetime(2018, 2, 7)
+client = "Software Imaging"
+t.get_tasks(start_date, end_date, client)
