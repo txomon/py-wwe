fcc548e64edddc809d6f58860e6dc90131de2b1e
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-05-22 21:03:05 +0100

    Refactor TogglAPI

--- wwe/toggl.py
+++ wwe/toggl.py
@@ -1,5 +1,4 @@
-# from tapioca_toggl import Toggl
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 # from wwe.config import import_config
 from wwe.toggl_api import TogglAPI
 # import pprint
@@ -10,11 +9,13 @@ from wwe.toggl_api import TogglAPI
 
 
 class TogglWrap:
-    def __init__(self, token="", timezone=""):
-        if token != "" and token is not None:
-            print("jey! token received! :D")
-            self.toggl = TogglAPI(api_token=token, timezone=timezone)
-            print("Toggl API client wrapper created!")
+    def __init__(self, token=""):
+        assert token
+        self.toggl = TogglAPI(api_token=token)
+
+    def get_today(self):
+        today_start = datetime.combine(date.today(), datetime.min.time())
+        return self.toggl.get_time_entries(start_date=today_start)
 
     def _client_by_id(self, client_id: int):
         if not hasattr(self, "_clients"):
@@ -27,18 +28,6 @@ class TogglWrap:
                 return c
 
         raise Exception(f"client {client_id} not found!")
-
-    def _project_by_id(self, project_id: int):
-        if not hasattr(self, "_projects"):
-            self.projects()
-        if not hasattr(self, "_projects"):
-            raise Exception('cannot fetch projects from Toggl')
-
-        for p in self._projects:
-            if p["id"] == project_id:
-                return p
-
-        return self._projects
 
     def _project_by_client(self, client_name: str):
         if not hasattr(self, "_clients"):
@@ -115,23 +104,3 @@ class TogglWrap:
                             entry["project"] = p["name"]
                             entry["client"] = p["client"]
                 return entries
-
-# TODO: remove stuff below this line and add unit tests
-
-
-# cfg = import_config('./config.json')
-# t = TogglWrap(token=cfg['toggl']['token'], timezone=cfg['toggl']['timezone'])
-# start_date = datetime(2018, 2, 5)
-# end_date = datetime(2018, 2, 7)
-# client = "Software Imaging"
-# kk = t.tasks(start_date, end_date, client)
-# pp = pprint.PrettyPrinter(indent=2)
-# for x in kk:
-#     xx = {
-#         "start": x.get("start"),
-#         "client": x.get("client"),
-#         "project": x.get("project"),
-#         "description": x.get("description"),
-#     }
-#     print('\n')
-#     pp.pprint(xx)
--- wwe/toggl_api.py
+++ wwe/toggl_api.py
@@ -1,82 +1,57 @@
 # Credit to Mosab Ibrahim <mosab.a.ibrahim@gmail.com>
 
 import requests
+import datetime
 
-from urllib.parse import urlencode
 from requests.auth import HTTPBasicAuth
+
+TOGGL_TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S'
+
+
+def toggl_format(ts: datetime.datetime):
+    if not ts:
+        return ''
+    tz = ts.strftime('%z')
+    if tz:
+        tz = f'{tz[:2]}:{tz[3:]}'
+    else:
+        tz = '+00:00'
+    return ts.strftime(TOGGL_TIMESTAMP_FORMAT) + tz
 
 
 class TogglAPI(object):
     """A wrapper for Toggl Api"""
 
-    def __init__(self, api_token, timezone):
-        self.api_token = api_token
-        self.timezone = timezone
+    def __init__(self, api_token):
+        self.session = requests.Session()
+        self.session.auth = HTTPBasicAuth(api_token, 'api_token')
+        self.session.headers = {'content-type': 'application/json'}
 
-    def _make_url(self, section='time_entries', params={}):
-        """Constructs and returns an api url to call with the section of the
-        API to be called and parameters defined by key/pair values in the
-        paramas dict. Default section is "time_entries" which evaluates to
-        "time_entries.json"
-
-        >>> t = TogglAPI('_SECRET_TOGGLE_API_TOKEN_')
-        >>> t._make_url(section='time_entries', params = {})
-        'https://www.toggl.com/api/v8/time_entries'
-
-        >>> t = TogglAPI('_SECRET_TOGGLE_API_TOKEN_')
-        >>> t._make_url(section='time_entries',
-                        params = {'start_date': '2010-02-05T15:42:46+02:00',
-                        'end_date': '2010-02-12T15:42:46+02:00'})
-        'https://www.toggl.com/api/v8/time_entries?start_date=2010-02-05T15%3A42%3A46%2B02%3A00%2B02%3A00&end_date=2010-02-12T15%3A42%3A46%2B02%3A00%2B02%3A00'
-        """
-
-        url = 'https://www.toggl.com/api/v8/{}'.format(section)
-        if len(params) > 0:
-            url = url + '?{}'.format(urlencode(params))
-        return url
-
-    def _query(self, url, method):
-        """Performs the actual call to Toggl API"""
-
-        url = url
-        headers = {'content-type': 'application/json'}
-        auth = HTTPBasicAuth(self.api_token, 'api_token')
-
-        if method == 'GET':
-            return requests.get(url, headers=headers, auth=auth)
-        elif method == 'POST':
-            return requests.post(url, headers=headers, auth=auth)
-        else:
-            raise ValueError('Undefined HTTP method "{}"'.format(method))
+    def get(self, section, params):
+        response = self.session.get(
+            f'https://www.toggl.com/api/v8/{section}', params=params)
+        return response.json()
 
     def clients(self, workspace_id=''):
         """Get Projects by Workspace ID"""
         section = f'workspaces/{workspace_id}/clients'
-        url = self._make_url(section=section)
-        r = self._query(url=url, method='GET')
-        return r.json()
+        return self.get(section=section)
 
-    def get_time_entries(self, start_date='', end_date='', timezone=''):
+    def get_time_entries(self, start_date: datetime.datetime='', end_date: datetime.datetime =''):
         """Get Time Entries JSON object from Toggl within a given start_date
         and an end_date with a given timezone"""
         p = {
-            'start_date': start_date + self.timezone,
-            'end_date': end_date + self.timezone
+            'start_date': toggl_format(start_date),
+            'end_date': toggl_format(end_date)
         }
-        url = self._make_url(section='time_entries',
-                             params=p)
-        r = self._query(url=url, method='GET')
-        return r.json()
+        return self.get(section='time_entries', params=p)
 
     def projects(self, workspace_id=''):
         """Get Projects by Workspace ID"""
         section = f'workspaces/{workspace_id}/projects'
-        url = self._make_url(section=section)
-        r = self._query(url=url, method='GET')
-        return r.json()
+        return self.get(section=section)
 
     def workspaces(self):
         """Get Workspaces"""
-        url = self._make_url(section='workspaces')
-        r = self._query(url=url, method='GET')
-        return r.json()
+        section = f'workspaces'
+        return self.get(section=section)
