f0ef8acb1279dbe73f30cba292506c01520ce107
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-05-23 19:49:39 +0100

    WIP

--- wwe/cli.py
+++ wwe/cli.py
@@ -1,7 +1,78 @@
 from pprint import pprint as print
 from wwe.toggl import TogglWrap
 from wwe.config import import_config
-from datetime import datetime
+from wwe.recorder import Task, BankHoliday, PersonalHoliday, Recorder
+from wwe.gov import gov_uk_bank_holidays_between
+import datetime
+
+
+def toggl_entry_to_task(entry: dict) -> Task:
+    start = entry.get('start')
+    end = entry.get('stop', datetime.now())
+    project = entry.get('project')
+    description = entry.get('description')
+    task = Task(start, end, project, description)
+    return task
+
+
+def toggl_entries_to_tasks(entries: set) -> set:
+    result = set()
+    for entry in entries:
+        task = toggl_entry_to_task(entry)
+        result.add(task)
+    return result
+
+
+def fetch_tasks(toggl_token: str, start: datetime) -> set:
+    t = TogglWrap(token=toggl_token)
+    toggl_tasks = t.tasks_from(start)
+    tasks = toggl_entries_to_tasks(toggl_tasks)
+    return tasks
+
+
+def gov_to_bank_holiday(gov_bank_holiday: str) -> set:
+    date = datetime.strptime(gov_bank_holiday)
+    bank_holiday = BankHoliday(date)
+    return bank_holiday
+
+
+def gov_to_bank_holidays(gov_bank_holidays: set) -> set:
+    bank_holidays = set()
+    for gov_bank_holiday in gov_bank_holidays:
+        bank_holiday = gov_to_bank_holiday(gov_bank_holiday)
+        bank_holidays.add(bank_holiday)
+    return bank_holidays
+
+
+def fetch_bank_holidays(start: datetime) -> set:
+    end = datetime.datetime.now()
+    gov_bank_holidays = gov_uk_bank_holidays_between(start, end)
+    bank_holidays = gov_to_bank_holidays(gov_bank_holidays)
+    return bank_holidays
+
+
+def fetch_personal_holidays(config_personal_holidays: list, start: datetime) -> set:
+    # bank_holidays = Gov
+    bank_holidays = set()
+    return bank_holidays
+
+
+def main2():
+    config = import_config('./config.json')
+    start_date = config['client']['start_date']
+    start = datetime.datetime.strptime(start_date, '%Y-%m-%dT%h:%M:%S.fZ')
+    personal_holidays = config['personal_holidays']
+    tasks = fetch_tasks(config['toggl_token'], start)
+    bank_holidays = fetch_bank_holidays(start)
+    personal_holidays = fetch_personal_holidays(start)
+
+    r = Recorder(config['working_day_hours'])
+    r.add(tasks)
+    r.add(bank_holidays)
+    r.add(personal_holidays)
+
+    summary = r.summary()
+    print(summary)
 
 
 def main():
